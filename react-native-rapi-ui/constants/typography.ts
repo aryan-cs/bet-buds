be9fd8a365df944029e56dac9247bdd13b0229f7
aryan-cs <aryan05g@gmail.com>
2024-07-22 16:13:23 -0500

    using roboto font now

---
+++ react-native-rapi-ui/constants/typography.ts
@@ -0,0 +1,25 @@
+const fontSize = {
+	h1: 36,
+	h2: 30,
+	h3: 24,
+	xl: 20,
+	lg: 16, // default
+	md: 14,
+	sm: 13,
+};
+
+const normal = {
+	light: 'Roboto-Light',
+	regular: 'Roboto-Regular', // default
+	medium: 'Roboto-Medium',
+	bold: 'Roboto-Bold',
+};
+
+const italic = {
+	light: 'Roboto-LightItalic',
+	regular: 'Roboto-RegularItalic',
+	medium: 'Roboto-MediumItalic',
+	bold: 'Roboto-BoldItalic',
+};
+
+export { fontSize, normal, italic };
